2b099c3271274a5e0f1aa2f2747f9cbf6828434c
rubing <rubingem@163.com>
2017-09-30 13:45:41 +0800

    process post request by bodyParser

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 const Koa = require('koa');
 // 注意require('koa-router')返回的是函数:
 const router =require('koa-router')();
+const bodyParser=require('koa-bodyParser');
 
 const app = new Koa();
-
+//必须在router之前注册
+app.use(bodyParser());
 //add router middleware
 app.use(router.routes())
 
@@ -22,8 +24,32 @@ router.get('/hello/:name', async (ctx, next) => {
     ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
 
+// router.get('/', async (ctx, next) => {
+//     ctx.response.body = '<h1>Index</h1>';
+// });
+
+//处理post请求
+
 router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>';
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
+});
+
+router.post('/signin', async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
 });
 
 app.listen(3000);
