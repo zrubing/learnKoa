12b8966a49cfafc0a8e218aa94bc0ad2747feda3
rubing <rubingem@163.com>
2017-09-30 17:01:44 +0800

    refactor code to controller.js middleware

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const Koa = require('koa');
 // 注意require('koa-router')返回的是函数:
 const router =require('koa-router')();
 const bodyParser=require('koa-bodyParser');
-const fs=require('fs');
 
 const app = new Koa();
 //必须在router之前注册
@@ -15,37 +14,7 @@ app.use(async (ctx, next) => {
     await next();
 });
 
-//add router middleware
-app.use(router.routes())
 
-function addMapping(router, mapping) {
-    for (var url in mapping) {
-        if (url.startsWith('GET ')) {
-            var path = url.substring(4);
-            router.get(path, mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-        } else if (url.startsWith('POST ')) {
-            var path = url.substring(5);
-            router.post(path, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-        } else {
-            console.log(`invalid URL: ${url}`);
-        }
-    }
-}
-
-function addControllers(router) {
-    var files = fs.readdirSync(__dirname + '/controllers');
-    var js_files = files.filter((f) => {
-        return f.endsWith('.js');
-    });
-    for (var f of js_files) {
-        console.log(`process controller: ${f}...`);
-        let mapping = require(__dirname + '/controllers/' + f);
-        addMapping(router, mapping);
-    }
-}
-addControllers(router);
 
 app.listen(3000);
 console.log('app started at port 3000');
