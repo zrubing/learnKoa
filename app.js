3a8ba30484c9fec6d9217b59f790105e138b8f39
rubing <rubingem@163.com>
2017-09-30 13:39:18 +0800

    add koa-router

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 'use strict'
 
 const Koa = require('koa');
+// 注意require('koa-router')返回的是函数:
+const router =require('koa-router')();
 
 const app = new Koa();
+
+//add router middleware
+app.use(router.routes())
+
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
 
 app.listen(3000);
 console.log('app started at port 3000');
