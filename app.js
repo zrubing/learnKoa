9cfe87ac9dfe0de520f61cc14d1a22e940df762e
rubing <rubingem@163.com>
2017-09-30 13:52:18 +0800

    move routers from app.js to controllers

--- app.js
+++ app.js
@@ -18,39 +18,7 @@ app.use(async (ctx, next) => {
     await next();
 });
 
-// add url-route:
-router.get('/hello/:name', async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
 
-// router.get('/', async (ctx, next) => {
-//     ctx.response.body = '<h1>Index</h1>';
-// });
-
-//处理post请求
-
-router.get('/', async (ctx, next) => {
-    ctx.response.body = `<h1>Index</h1>
-        <form action="/signin" method="post">
-            <p>Name: <input name="name" value="koa"></p>
-            <p>Password: <input name="password" type="password"></p>
-            <p><input type="submit" value="Submit"></p>
-        </form>`;
-});
-
-router.post('/signin', async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
-    }
-});
 
 app.listen(3000);
 console.log('app started at port 3000');
